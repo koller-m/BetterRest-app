7d71bf0e3996771979b2bfdfb265b219d5110aca
koller-m <mkoller@eml.cc>
2021-05-11 18:09:17 -0600

    Reset changes

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,12 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
-    
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-            Text("\(sleepAmount, specifier: "%g") hours")
-        }
+        // reset changes
+        Text("Hello World")
     }
 }
 
