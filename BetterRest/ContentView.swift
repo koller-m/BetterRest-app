f31462667f02d111817ea66d7040f60bf0ae3ca0
koller-m <mkoller@eml.cc>
2021-05-12 18:03:02 -0600

    Add computed property defaultWakeTime Change VStack to Form

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
@@ -16,22 +16,35 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
                 Text("When do you want to wake up?")
                     .font(.headline)
                 
                 DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
+                    .datePickerStyle(WheelDatePickerStyle())
+                }
                 
+                VStack(alignment: .leading, spacing: 0) {
                 Text("Desired amount of sleep")
                     .font(.headline)
                 
                 Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
                     Text("\(sleepAmount, specifier: "%g") hours")
                 }
+                }
                 
+                VStack(alignment: .leading, spacing: 0) {
                 Text("Daily coffee intake")
                     .font(.headline)
                 
@@ -41,6 +54,7 @@ struct ContentView: View {
                     } else {
                         Text("\(coffeeAmount) cups")
                     }
+                }
                 }
             }
             .navigationBarTitle("BetterRest")
